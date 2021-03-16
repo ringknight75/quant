8a42e52d8cc7e6880fb58ab6a5c1167c4abbe932
공순재 <hanwhalife@gongsunjaeui-MacBookPro.local>
2021-03-16 10:45:08 +0900

    수정

--- get_dollar_data.R
+++ get_dollar_data.R
@@ -3,32 +3,62 @@
 # 
 # 
 #===========================================================================
-
-
 library(quantmod)
+library(tidyverse)
 
-
-
+(to_day   <- Sys.Date() - 1)
+(from_day <- Sys.Date() - 367)
 
 #===========================================================================
 # 1) 원달러 환율 
 #===========================================================================
-won_dolloar <- getSymbols(Symbols="USD/KRW", 
-                         src = "yahoo", 
-                         from=  Sys.Date() - 180, 
-                         to =  Sys.Date(), auto.assign = FALSE)
-plot(usdkrw_xts)
+won_dolloar_xts <- getSymbols("KRW=X", src = "yahoo", auto.assign = FALSE)
+#won_dolloar_xts <- getSymbols("DEXKOUS", src = "FRED", auto.assign = FALSE)
+head(won_dolloar_xts)
+tail(won_dolloar_xts)
+
 
 
 #===========================================================================
 # 2) 달러 지수 
 #===========================================================================
-dallor_idx <- getSymbols('DXY',   
-                         src = 'yahoo', 
-                         from = '2020-03-15', to = '2021-03-04', 
-                         auto.assign = FALSE) 
-tail(dallor_idx)
-head(dallor_idx)
+dallor_idx_xts <- getSymbols("DX-Y.NYB",  src = "yahoo", auto.assign = FALSE)
+tail(dallor_idx_xts)
+head(dallor_idx_xts)
+
+
+#===========================================================================
+# 3) xts -> dataframe 변경 
+#===========================================================================
+#한꺼번에 Adjusted만 남겨놓고 dataframe형식에 date를 넣어보자 
+
+won_dolloar_dt <- data.frame(date = index(won_dolloar_xts), 
+                             adjusted = won_dolloar_xts$`KRW=X.Adjusted`, 
+                             row.names = NULL) %>% 
+  as_tibble() %>% 
+  complete(date, fill = list(KRW.X.Adjusted = NA))
+  
+tail(won_dolloar_dt, 20)
+
+dallor_idx_dt <- data.frame(date = index(dallor_idx_xts), 
+                            adjusted = dallor_idx_xts$`DX-Y.NYB.Adjusted`, 
+                            row.names = NULL) %>%
+  as_tibble() %>% 
+  complete(date, fill = list(DX.Y.NYB.Adjusted = NA))
+
+tail(dallor_idx_dt, 20)
+
+
+#===========================================================================
+# 4) dataframe JOIN (full join)
+#===========================================================================
+
+join_full_dt <-  full_join(dallor_idx_dt, won_dolloar_dt, by = "date")
+head(join_full_dt)
+tail(join_full_dt)
+
+
+
 
 
 
