760d04df1fd33d6f2c861727d27fd3d02a8dc6ed
공순재 <hanwhalife@gongsunjaeui-MacBookPro.local>
2021-03-16 13:29:18 +0900

    수정

--- get_dollar_data.R
+++ get_dollar_data.R
@@ -1,10 +1,11 @@
 #===========================================================================
-# 적절한 달러 매매 시기 확보 
-# 
-# 
+# 작성목적 : 적절한 달러 매매 시기 확보 
+# 작 성 자 : 반지기사 
+# 작성일시 : 2021년 3월 16일 ~ 
 #===========================================================================
 library(quantmod)
 library(tidyverse)
+library(lubridate)
 
 (to_day   <- Sys.Date() - 1)
 (from_day <- Sys.Date() - 367)
@@ -13,8 +14,8 @@ library(tidyverse)
 # 1) 원달러 환율 
 #===========================================================================
 won_dolloar_xts <- getSymbols("KRW=X", src = "yahoo", auto.assign = FALSE)
-#won_dolloar_xts <- getSymbols("DEXKOUS", src = "FRED", auto.assign = FALSE)
-head(won_dolloar_xts)
+#won_dolloar_xts <- getSymbols("DEXKOUS", src = "FRED", auto.assign = FALSE)  # 4일전 데이터 적재됨 
+# head(won_dolloar_xts)
 tail(won_dolloar_xts)
 
 
@@ -23,8 +24,8 @@ tail(won_dolloar_xts)
 # 2) 달러 지수 
 #===========================================================================
 dallor_idx_xts <- getSymbols("DX-Y.NYB",  src = "yahoo", auto.assign = FALSE)
+# head(dallor_idx_xts)
 tail(dallor_idx_xts)
-head(dallor_idx_xts)
 
 
 #===========================================================================
@@ -32,31 +33,125 @@ head(dallor_idx_xts)
 #===========================================================================
 #한꺼번에 Adjusted만 남겨놓고 dataframe형식에 date를 넣어보자 
 
-won_dolloar_dt <- data.frame(date = index(won_dolloar_xts), 
+won_dolloar_dt <- data.frame(dates = index(won_dolloar_xts), 
                              adjusted = won_dolloar_xts$`KRW=X.Adjusted`, 
                              row.names = NULL) %>% 
   as_tibble() %>% 
-  complete(date, fill = list(KRW.X.Adjusted = NA))
-  
-tail(won_dolloar_dt, 20)
+  complete(dates = seq.Date(from = min(dates),
+                            to = max(dates),
+                            by = "day"))
+# tail(won_dolloar_dt, 20)
 
-dallor_idx_dt <- data.frame(date = index(dallor_idx_xts), 
+dallor_idx_dt <- data.frame(dates = index(dallor_idx_xts), 
                             adjusted = dallor_idx_xts$`DX-Y.NYB.Adjusted`, 
                             row.names = NULL) %>%
   as_tibble() %>% 
-  complete(date, fill = list(DX.Y.NYB.Adjusted = NA))
+  complete(dates = seq.Date(from = min(dates),
+                            to = max(dates),
+                            by = "day"))
 
-tail(dallor_idx_dt, 20)
+
+# tail(dallor_idx_dt, 20)
 
 
 #===========================================================================
 # 4) dataframe JOIN (full join)
 #===========================================================================
 
-join_full_dt <-  full_join(dallor_idx_dt, won_dolloar_dt, by = "date")
-head(join_full_dt)
-tail(join_full_dt)
+join_full_dt <-  full_join(dallor_idx_dt, won_dolloar_dt, by = "dates") %>% 
+  mutate(week     = wday(dates),
+         day_week = week(dates),
+         before_year = dates-365) %>% 
+  rename(dollar_idx = DX.Y.NYB.Adjusted,
+         won_dolloar_price = KRW.X.Adjusted) %>% 
+  filter(week %in% 2:6) %>%   #1:일요일, 7:토요일 제외
+  filter(!is.na(dollar_idx), !is.na(won_dolloar_price)) %>%  # NA값 제외 
+  arrange(desc(dates))
 
+head(join_full_dt, 100)
+
+
+
+#===========================================================================
+# 5) 통계
+#===========================================================================
+join_full_dt %>% 
+  summarise(max_won_dolloar_price = max(won_dolloar_price),
+            min_won_dolloar_price = min(won_dolloar_price),
+            mean_won_dolloar_price = mean(won_dolloar_price))
+# max_won_dolloar_price min_won_dolloar_price mean_won_dolloar_price
+#                 <dbl>                 <dbl>                  <dbl>
+#1                 1571.                  890.                  1120.
+
+join_full_dt %>% 
+  summarise(max_dollar_idx = max(dollar_idx),
+            min_dollar_idx = min(dollar_idx),
+            mean_dollar_idx = mean(dollar_idx))
+# max_dollar_idx min_dollar_idx mean_dollar_idx
+#          <dbl>          <dbl>           <dbl>
+#1           103.           71.3            87.0
+
+
+
+#===========================================================================
+# 6) 달러 갭 비율 계산하기 
+#    달러지수 / 원 달러 환율 * 100 
+#===========================================================================
+
+join_cal_1_dt <- join_full_dt %>% 
+  mutate(dollar_gap = dollar_idx/won_dolloar_price * 100)
+
+head(join_cal_1_dt)
+
+
+
+#===========================================================================
+# 6) 52주 평균 달러 갭 비율 계산하기 
+#    현재의 달러지수 / 52주 평균 달러 갭 비율 * 100 
+#===========================================================================
+join_cal_2_dt <- join_cal_1_dt
+join_cal_2_dt$mean_52week_gap <- 0
+
+
+join_cal_2_dt %>% 
+  filter(dates <= as.Date('2021-03-12'),  dates >=  as.Date('2020-03-12')) %>% 
+  select(dollar_gap) %>% 
+  summarise(mean_52week_gap = mean(dollar_gap))
+
+
+for(i in 1:nrow(join_cal_2_dt)) {
+  join_cal_2_dt[i,8] <- join_cal_1_dt %>% 
+    filter(dates <=  as.Date(unlist(join_cal_2_dt[i,1])),  dates >= as.Date(unlist(join_cal_2_dt[i,1]-365))) %>% 
+    select(dollar_gap) %>% 
+    summarise(mean_52week_gap = mean(dollar_gap))
+}
+
+# as.Date(unlist(join_cal_2_dt[7,1]))
+# as.Date(unlist(join_cal_2_dt[7,1]-365))
+head(join_cal_2_dt, 50)
+tail(join_cal_2_dt)
+
+join_cal_2_dt %>% distinct(mean_52week_gap)
+
+
+
+
+#===========================================================================
+# 7) 달러 최적 매수 타이밍 판단하기  
+#===========================================================================
+#
+#    7-1) 원달러 환율 하락 (각각 또는 두가지 상황이 발생될 수 있음)
+#         - 달러 가치의 하락 -> 달러지수 값이 하락 
+#         - 원화 가치의 상승 -> 달러지수는 동일하나 원화가치가 상승 할 경우 
+#
+#    7-2) 달러지수 (달러의 절대가치): dollar_idx 
+#         - 1993년 3월 기준 100을 설정 (6개국 기준)
+#
+#    7-3) 원화 가치 
+#
+#
+# 
+#===========================================================================
 
 
 
