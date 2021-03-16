93d42bd8717d575ebc6fad2e3059e5e5244a9a43
공순재 <hanwhalife@gongsunjaeui-MacBookPro.local>
2021-03-16 14:47:46 +0900

    수정

--- get_dollar_data.R
+++ get_dollar_data.R
@@ -68,7 +68,7 @@ join_full_dt <-  full_join(dallor_idx_dt, won_dolloar_dt, by = "dates") %>%
   filter(!is.na(dollar_idx), !is.na(won_dolloar_price)) %>%  # NA값 제외 
   arrange(desc(dates))
 
-head(join_full_dt, 100)
+# head(join_full_dt, 100)
 
 
 
@@ -101,7 +101,7 @@ join_full_dt %>%
 join_cal_1_dt <- join_full_dt %>% 
   mutate(dollar_gap = dollar_idx/won_dolloar_price * 100)
 
-head(join_cal_1_dt)
+# head(join_cal_1_dt)
 
 
 
@@ -146,12 +146,6 @@ join_cal_3_dt <- join_cal_2_dt %>% mutate(before_dollar_idx = lead(dollar_idx, n
 #===========================================================================
 # 8) 시각화 하기  
 #===========================================================================
-join_cal_3_dt %>% select(dates, dollar_idx, won_dolloar_price, mean_52week_gap) %>% 
-  gather(dollar_idx:mean_52week_gap, key = 'key', value = 'value') %>% 
-  filter(dates >= '2020-01-01') %>% 
-  ggplot(aes(x = dates, y = value)) +
-  geom_line() +
-  facet_wrap(~key)
 
 join_cal_3_dt %>% select(dates, dollar_idx, won_dolloar_price, mean_52week_gap) %>% 
   mutate(adjust_won_dolloar_price = won_dolloar_price * 0.1,
@@ -164,7 +158,7 @@ join_cal_3_dt %>% select(dates, dollar_idx, won_dolloar_price, mean_52week_gap) 
   scale_y_log10()
 
 
-join_cal_3_dt 
+# join_cal_3_dt 
 
 #===========================================================================
 # 9) 달러 최적 매수 타이밍 판단하기  
@@ -177,12 +171,50 @@ join_cal_3_dt
 #    7-2) 달러지수 (달러의 절대가치): dollar_idx 
 #         - 1993년 3월 기준 100을 설정 (6개국 기준)
 #
-#    7-3) 원화 가치 
-#
-#
+#    7-3) 매수 타이밍 
+#         조건 1) 현재 환율이 52주 평균 환율 보다 낮을 때 
+#         조건 2) 현재 달러지수가 52주 평균 달러 지수 보다 낮을 때 
+#         조건 3) 현재 달러 Gap비율이 52주 평균 달러 Gap비율 보다 높을 때  
+#         조건 4) 현재 환율이 적정 활율 보다 낮을 때 
+#                 적정환율 = 현재 달러지수 / 52주 평균달러 Gap비율 * 100 
 # 
 #===========================================================================
 
+# 현재 max기준 52주 평균 환율 
+max_date <- join_cal_3_dt %>% summarise(max_date = max(dates)) %>% unlist()
+
+mean_won_dolloar_price <- join_cal_3_dt %>% 
+  filter(dates <= max_date, dates >= max_date - 365) %>% 
+  summarise(mean_won_dolloar_price = mean(won_dolloar_price)) %>% unlist()
+
+mean_dollar_idx <- join_cal_3_dt %>% 
+  filter(dates <= max_date, dates >= max_date - 365) %>% 
+  summarise(mean_dollar_idx = mean(dollar_idx)) %>% unlist()
+
+
+cat("================================================================================ ")
+head(join_cal_3_dt, 1)
+
+
+# 조건1 : 현재 환율이 52주 평균 환율 보다 낮을 때 
+cat("현재 달러 가격 : ", unlist(join_cal_3_dt[1, 4]))
+cat("52주 평균 환율 : ", mean_won_dolloar_price)
+if(unlist(join_cal_3_dt[1, 4])  <  mean_won_dolloar_price) print("Condition 1 Yes")  else ("Condition 1 No") 
+
+# 조건 2) 현재 달러지수가 52주 평균 달러 지수 보다 낮을 때 
+cat("현재 달러 지수 : ", unlist(join_cal_3_dt[1, 2]))
+cat("52주 평균 달러 지수 : ", mean_dollar_idx )
+if(unlist(join_cal_3_dt[1, 2])  <  mean_dollar_idx) print("Condition 2 Yes")  else ("Condition 2 No") 
+
+#조건 3) 현재 달러 Gap비율이 52주 평균 달러 Gap비율 보다 높을 때
+cat("현재 달러 갭 비율  : ", unlist(join_cal_3_dt[1, 6]))
+cat("52주 평균 달러 갭 비율  : ", unlist(join_cal_3_dt[1, 7]))
+if(unlist(join_cal_3_dt[1, 6])  >  unlist(join_cal_3_dt[1, 7])) print("Condition 3 Yes")  else ("Condition 3 No") 
+
+#조건 4) 현재 환율이 적정 활율 보다 낮을 때 
+cat("현재 달러 가격 : ", unlist(join_cal_3_dt[1, 4]))
+cat("적정 환율  : ", unlist(join_cal_3_dt[1, 2]) / unlist(join_cal_3_dt[1, 7]) * 100)
+if(unlist(join_cal_3_dt[1, 4])  <  (unlist(join_cal_3_dt[1, 2]) / unlist(join_cal_3_dt[1, 7]) * 100)) print("Condition 4 Yes")  else ("Condition 4 No") 
 
 
 
